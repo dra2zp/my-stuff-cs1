0076c8bc9f7286535c51b048678a5072c7833abe
Denny Anderson <dra2zp@virginia.edu>
2015-11-08 15:51:46 -0500

    initial commit

--- code/python/hs.py
+++ code/python/hs.py
@@ -1,13 +1,15 @@
 def hs(n):
-    x = 0
-    while n > 1:
-        print(n)
-        if (n % 2 == 0):
-            n = (n // 2)
+    if (n < 1):
+        print("Error: n must be a positive integer.")
+    else:
+        x = 0
+        while n > 1:
+            print(n)
+            if (n % 2 == 0):
+                n = (n // 2)
+            else:
+                n = (3 * n + 1)
             x = x + 1
-        else:
-            n = (3 * n + 1)
-            x = x + 1
-    print(1)
-    x = x + 1
-    print(x)
+        print(1)
+        x = x + 1
+        print("Length: " + str(x))
